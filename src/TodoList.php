0adfac205f4ab3719934093b1fb3a1e6186ed19e
psytwr <psytwr@exmail.nottingham.ac.uk>
2021-03-19 15:33:46 +0000

    added the skeleton to the repository

---
+++ src/TodoList.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TodoApi;
+
+class TodoList
+{
+
+    private $id;
+
+    private $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+}
